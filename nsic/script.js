4fa48ec7cd1216f22801b40c486ef0848528353b
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2023-12-07 18:42:49 +0530

    Update nsic/script.js
    This is done to change all the files to png format as they are smaller than jpg and more flexible

--- nsic/script.js
+++ nsic/script.js
@@ -1,5 +1,5 @@
 
 function loadRandomBackground(){
     var img_id = Math.floor(Math.random() * 8) + 1;
-    document.getElementById("img-background").style.backgroundImage = "url('images/image" + img_id + ".jpg')";
+    document.getElementById("img-background").style.backgroundImage = "url('images/image" + img_id + ".png')";
 }
